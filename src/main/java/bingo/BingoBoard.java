88cd81019af7bef99e241ecf21ce1e03099a37dc
Peter Fichtner <Peter.Fichtner@atruvia.de>
2022-03-31 17:39:34 +0200

    attribute name fixed

--- src/main/java/bingo/BingoBoard.java
+++ src/main/java/bingo/BingoBoard.java
@@ -2,25 +2,25 @@ package bingo;
 
 public class BingoBoard {
 
-	private final String[][] cell;
+	private final String[][] cells;
 	private final boolean[][] marked;
 
 	public BingoBoard(int width, int height) {
-		this.cell = new String[width][height];
+		this.cells = new String[width][height];
 		this.marked = new boolean[width][height];
 	}
 
 	public void defineCell(int x, int y, String value) {
-		if (cell[x][y] != null) {
+		if (cells[x][y] != null) {
 			throw new IllegalStateException("cell already defined");
 		}
-		for (int c = 0; c < cell.length; c++) {
-			for (int r = 0; r < cell[c].length; r++) {
-				if (value.equals(cell[c][r]))
+		for (int c = 0; c < cells.length; c++) {
+			for (int r = 0; r < cells[c].length; r++) {
+				if (value.equals(cells[c][r]))
 					throw new IllegalStateException(value + " already present at " + c + "," + r);
 			}
 		}
-		cell[x][y] = value;
+		cells[x][y] = value;
 	}
 
 	public void markCell(int x, int y) {
@@ -35,7 +35,7 @@ public class BingoBoard {
 	}
 
 	public boolean isInitialzed() {
-		for (String[] row : cell) {
+		for (String[] row : cells) {
 			for (String col : row) {
 				if (col == null)
 					return false;
