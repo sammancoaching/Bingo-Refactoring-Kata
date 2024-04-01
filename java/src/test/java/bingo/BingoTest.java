595ad4ae99b79c41a43c6967970c27dc32d1cc5f
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-04-01 18:46:43 +0200

    using the same test class naming scheme like csharp code

--- java/src/test/java/bingo/BingoTest.java
+++ java/src/test/java/bingo/BingoTest.java
@@ -5,40 +5,45 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * This class is identical to {@link BingoTest} but all BDD-style methods are
+ * inlined (and thus this test does depend at multiple points on the
+ * implementation details of {@link BingoBoard})
+ */
 class BingoTest {
 
 	BingoBoard board;
 
 	@Test
-	void aNewlyCreatedBoardIsNotInitialized() {
-		givenBingoBoardOfSize(1, 1);
-		thenBoardIsNotInitialized();
+	void anNewlyCreatedBoardIsNotInitialized() {
+		board = new BingoBoard(1, 1);
+		assertThat(board.isInitialized()).isFalse();
 	}
 
 	@Test
-	void whenAllFieldsAreSetTheBoardIsInitialized() {
+	void whenAllFieldsAreSetTheBoarIsInitialized() {
 		String anyValue = "42";
-		givenBingoBoardOfSize(1, 1);
-		whenCellIsDefined(0, 0, anyValue);
-		thenBoardIsInitialized();
+		board = new BingoBoard(1, 1);
+		board.defineCell(0, 0, anyValue);
+		assertThat(board.isInitialized()).isTrue();
 	}
 
 	@Test
 	void whenAllFieldsOnRectangularBoardAreSetItIsInitialized() {
 		String one = "one, two, three";
 		String two = "Bingo cells can contain any text";
-		givenBingoBoardOfSize(1, 2);
-		whenCellIsDefined(0, 0, one);
-		whenCellIsDefined(0, 1, two);
-		thenBoardIsInitialized();
+		board = new BingoBoard(1, 2);
+		board.defineCell(0, 0, one);
+		board.defineCell(0, 1, two);
+		assertThat(board.isInitialized()).isTrue();
 	}
 
 	@Test
 	void aDefinedCellCantBeRedefinedEvenIfItsTheSameValue() {
 		String anyValue = "42";
-		givenBingoBoardOfSize(1, 1);
-		whenCellIsDefined(0, 0, anyValue);
-		assertThatThrownBy(() -> whenCellIsDefined(0, 0, anyValue)) //
+		board = new BingoBoard(1, 1);
+		board.defineCell(0, 0, anyValue);
+		assertThatThrownBy(() -> board.defineCell(0, 0, anyValue)) //
 				.isInstanceOf(RuntimeException.class) //
 				.hasMessageContaining("already defined");
 	}
@@ -46,17 +51,17 @@ class BingoTest {
 	@Test
 	void duplicateCellsAreNotAllowed() {
 		String anyValue = "42";
-		givenBingoBoardOfSize(2, 2);
-		whenCellIsDefined(0, 1, anyValue);
-		assertThatThrownBy(() -> whenCellIsDefined(1, 0, anyValue)) //
+		board = new BingoBoard(2, 2);
+		board.defineCell(0, 1, anyValue);
+		assertThatThrownBy(() -> board.defineCell(1, 0, anyValue)) //
 				.isInstanceOf(RuntimeException.class) //
 				.hasMessageContaining(anyValue + " already present at 0,1");
 	}
 
 	@Test
 	void aNonInitializedBoardCannotBeMarked() {
-		givenBingoBoardOfSize(1, 1);
-		assertThatThrownBy(() -> whenCellIsMarked(0, 0)) //
+		board = new BingoBoard(1, 1);
+		assertThatThrownBy(() -> board.markCell(0, 0)) //
 				.isInstanceOf(RuntimeException.class) //
 				.hasMessageContaining("not initialized");
 	}
@@ -64,38 +69,10 @@ class BingoTest {
 	@Test
 	void whenAllCellGetsMarkedItIsMarked() {
 		String anyValue = "42";
-		givenBingoBoardOfSize(1, 1);
-		whenCellIsDefined(0, 0, anyValue);
-		whenCellIsMarked(0, 0);
-		thenCellIsMarked(0, 0);
-	}
-
-	void givenBingoBoardOfSize(int width, int height) {
-		board = new BingoBoard(width, height);
-	}
-
-	void whenCellIsDefined(int x, int y, String value) {
-		board.defineCell(x, y, value);
-	}
-
-	void whenCellIsMarked(int x, int y) {
-		board.markCell(x, y);
-	}
-
-	void thenBoardIsNotInitialized() {
-		assertThat(boardInitializeState()).isFalse();
-	}
-
-	void thenBoardIsInitialized() {
-		assertThat(boardInitializeState()).isTrue();
-	}
-
-	boolean boardInitializeState() {
-		return board.isInitialized();
-	}
-
-	void thenCellIsMarked(int x, int y) {
-		assertThat(board.isMarked(x, y)).isTrue();
+		board = new BingoBoard(1, 1);
+		board.defineCell(0, 0, anyValue);
+		board.markCell(0, 0);
+		assertThat(board.isMarked(0, 0)).isTrue();
 	}
 
 }
